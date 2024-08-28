d3c1888c25f0a7e5feae3d0e85e1d5008093d67f
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-08-28 12:47:21 +0200

    .

--- CapstoneProject-Autolavaggi/Models/Autolavaggio.cs
+++ CapstoneProject-Autolavaggi/Models/Autolavaggio.cs
@@ -7,7 +7,7 @@ namespace CapstoneProject_Autolavaggi.Models
     public class Autolavaggio
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int Idd { get; set; }
         [Required]
         public string Nome { get; set; }
         [Required]
