4b77b2f08687375a79d397c643997bbbfdbf76bf
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-09-02 11:34:35 +0200

    funziona Modifica autolavaggio funzionante

--- CapstoneProject-Autolavaggi/Controllers/AutolavaggioController.cs
+++ CapstoneProject-Autolavaggi/Controllers/AutolavaggioController.cs
@@ -22,7 +22,7 @@ namespace CapstoneProject_Autolavaggi.Controllers
             {
                 Tipi = await _ctx.Tipi.ToListAsync(),
                 Servizi = await _ctx.Servizi.ToListAsync(),
-                
+
             };
 
             return View(viewmodel);
@@ -46,12 +46,12 @@ namespace CapstoneProject_Autolavaggi.Controllers
                     Immagine = model.Autolavaggio.Immagine,
                     OrariDescrizione = model.Autolavaggio.OrariDescrizione,
                     TipoNome = model.Autolavaggio.TipoNome,
-                    Servizi= await _ctx.Servizi
+                    Servizi = await _ctx.Servizi
                     .Where(s => model.SelectedServizi.Contains(s.Id))
                     .ToListAsync()
-                    
+
                 };
-            
+
                 _ctx.Autolavaggi.Add(autolavaggio);
                 await _ctx.SaveChangesAsync();
 
@@ -104,9 +104,111 @@ namespace CapstoneProject_Autolavaggi.Controllers
 
 
 
-        public IActionResult Index()
+        public async Task<IActionResult> DettagliAutolavaggioGestore(int id)
         {
-            return View();
+            var autolavaggio = await _ctx.Autolavaggi
+                .Include(a => a.Servizi)
+                .Include(a => a.Tipo)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (autolavaggio == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new AutolavaggioViewModel
+            {
+                Autolavaggio = autolavaggio,
+                Servizi = autolavaggio.Servizi,
+                Tipi = new List<Tipo> { autolavaggio.Tipo },
+                Recensioni = await _ctx.Recensioni.Where(r => r.AutolavaggioId == id).ToListAsync(),
+                Prenotazioni = await _ctx.Prenotazioni.Where(p => p.AutolavaggioId == id).ToListAsync()
+            };
+
+            return View(viewModel);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ModificaAutolavaggio(int id)
+        {
+            var autolavaggio = await _ctx.Autolavaggi
+                .Include(a => a.Servizi)
+                .Include(a => a.Tipo)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (autolavaggio == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new AutolavaggioViewModel
+            {
+                Autolavaggio = autolavaggio,
+                Servizi = await _ctx.Servizi.ToListAsync(),
+                Tipi = await _ctx.Tipi.ToListAsync(),
+                SelectedServizi = autolavaggio.Servizi.Select(s => s.Id).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ModificaAutolavaggio(AutolavaggioViewModel model)
+        {
+           
+                var autolavaggio = await _ctx.Autolavaggi
+                    .Include(a => a.Servizi) // Carica anche i servizi per la rimozione e aggiunta
+                    .FirstOrDefaultAsync(a => a.Id == model.Autolavaggio.Id);
+
+                if (autolavaggio == null)
+                {
+                    return NotFound();
+                }
+
+                // Aggiorna i dati dell'autolavaggio
+                autolavaggio.Nome = model.Autolavaggio.Nome;
+                autolavaggio.Via = model.Autolavaggio.Via;
+                autolavaggio.Città = model.Autolavaggio.Città;
+                autolavaggio.CAP = model.Autolavaggio.CAP;
+                autolavaggio.Telefono = model.Autolavaggio.Telefono;
+                autolavaggio.Descrizione = model.Autolavaggio.Descrizione;
+                autolavaggio.Immagine = model.Autolavaggio.Immagine;
+                autolavaggio.OrariDescrizione = model.Autolavaggio.OrariDescrizione;
+                autolavaggio.TipoNome = model.Autolavaggio.TipoNome;
+
+                // Aggiorna i servizi
+                var serviziToRemove = autolavaggio.Servizi.Where(s => !model.SelectedServizi.Contains(s.Id)).ToList();
+                var serviziToAdd = await _ctx.Servizi.Where(s => model.SelectedServizi.Contains(s.Id)).ToListAsync();
+
+                // Rimuove i servizi non selezionati
+                foreach (var servizio in serviziToRemove)
+                {
+                    autolavaggio.Servizi.Remove(servizio);
+                }
+
+                // Aggiunge i nuovi servizi
+                foreach (var servizio in serviziToAdd)
+                {
+                    if (!autolavaggio.Servizi.Contains(servizio))
+                    {
+                        autolavaggio.Servizi.Add(servizio);
+                    }
+                }
+
+                // Salva le modifiche nel database
+                await _ctx.SaveChangesAsync();
+
+                return RedirectToAction("DettagliAutolavaggio", new { id = autolavaggio.Id });
+            
+
+            // Se il modello non è valido, ricarica le liste dei servizi e dei tipi
+            model.Servizi = await _ctx.Servizi.ToListAsync();
+            model.Tipi = await _ctx.Tipi.ToListAsync();
+
+            return View(model);
+        }
+
+
     }
 }
---
+++ CapstoneProject-Autolavaggi/Models/PrenotazioniViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CapstoneProject_Autolavaggi.Models
+{
+    public class PrenotazioniViewModel
+    {
+        public Autolavaggio Autolavaggio { get; set; }
+        public List<Prenotazione> Prenotazioni { get; set; } = new List<Prenotazione>();
+    }
+}
